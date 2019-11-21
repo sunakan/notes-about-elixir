be80d5c8397b6ea5a037231dbb9e84da96936107
sunakan <sunakan03@gmail.com>
2019-11-21 20:19:26 +0900

    [Add] ファイル分割、lib/以下も勝手に読み込むっぽい?

--- sunabako/spec/hello_world_spec.exs
+++ sunabako/spec/hello_world_spec.exs
@@ -1,14 +1,7 @@
-defmodule HelloWorld do
-  def hello(arg) do
-    return :world if arg.nil?
-    "Hello, #{arg}"
-  end
-end
-
-defmodule HelloWorldSpec do
+defmodule SunabakoSpec do
   use ESpec
-  it do: expect HelloWorld.hello()
+  it do: expect Sunabako.hello()
                 |> to(eq :world)
-  it do: expect HelloWorld.hello("dd511805")
+  it do: expect Sunabako.hello("dd511805")
                 |> to(eq "Hello, dd511805")
 end
